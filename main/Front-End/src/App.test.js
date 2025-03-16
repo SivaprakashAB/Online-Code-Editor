477327052be93f9e944cb9b9914eaedfcb46c7ad
SivaprakashAB <128163582+SivaprakashAB@users.noreply.github.com>
2025-03-16 18:46:11 +0530

    Delete main/Front-End directory

--- main/Front-End/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Online Code Editor title', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/Online Code Editor/i);
-  expect(titleElement).toBeInTheDocument();
-});
